d6fbc0e412ecacecca50a93feb1f9e123bbd5da2
hothaofaz <hothaifazoubi@gmail.com>
2022-10-13 06:30:18 +0300

    this

--- Service/ECGService.py
+++ Service/ECGService.py
@@ -1,13 +1,18 @@
 from AppConfig.DBConnector import *
-
+from werkzeug import security
 
 def save_wav(id, wav):
     try:
         cur, conn = get_connection()
-        print('connected to database ECK ')
-        command = f"INSERT INTO ECG (patient_id , wav ) VALUES ( '{id}',{wav}) ;"
+        print('connected to database --- ECK ')
+        print(type(wav))
+
+        with open('www.wav', 'wb') as w:
+            w.write()
+        command = f" INSERT INTO ECG (id , wav ) VALUES ({id}, {wav} ) ;"
         cur.execute(command)
         conn.commit()
         return True
     except Exception as e:
         print(e)
+        return False
